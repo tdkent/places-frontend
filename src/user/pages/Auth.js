7c98a4f2bc39fbb561b0b0a1251a73f8f7f995a1
Tim Kent <timdekent@gmail.com>
2022-10-19 21:11:28 -0700

    update user auth to send register post request to backend

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,40 +1,71 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext } from 'react'
 
-import Card from "../../shared/components/UIElements/Card";
-import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button";
-import "./Auth.css";
-import {
-  VALIDATOR_EMAIL,
-  VALIDATOR_MINLENGTH,
-  VALIDATOR_REQUIRE,
-} from "../../shared/util/validators";
-import { useForm } from "../../shared/hooks/form-hook";
-import { AuthContext } from "../../shared/context/auth-context";
+import Card from '../../shared/components/UIElements/Card'
+import Input from '../../shared/components/FormElements/Input'
+import Button from '../../shared/components/FormElements/Button'
+import './Auth.css'
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators'
+import { useForm } from '../../shared/hooks/form-hook'
+import { AuthContext } from '../../shared/context/auth-context'
 
 const Auth = () => {
-  const [isLogIn, setIsLogIn] = useState(true);
+  const [isLogIn, setIsLogIn] = useState(true)
   const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
-        value: "",
+        value: '',
         isValid: false,
       },
       password: {
-        value: "",
+        value: '',
         isValid: false,
       },
     },
     false
-  );
-  
+  )
+
   const auth = useContext(AuthContext)
 
-  const authSubmitHandler = (e) => {
-    e.preventDefault();
-    console.log(formState);
+  const authSubmitHandler = async (e) => {
+    e.preventDefault()
+    if (isLogIn) {
+      try {
+        const response = await fetch('http://localhost:4000/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        })
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      try {
+        const response = await fetch('http://localhost:4000/api/users/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        })
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
     auth.login()
-  };
+  }
 
   const switchModeHandler = () => {
     if (!isLogIn) {
@@ -44,56 +75,57 @@ const Auth = () => {
           name: undefined,
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
-      );
+      )
     } else {
       setFormData(
         {
           ...formState.inputs,
           name: {
             value: '',
-            isValid: false
-          }
-        }, false
+            isValid: false,
+          },
+        },
+        false
       )
     }
-    setIsLogIn((prev) => !prev);
-    console.log("swiched mode to", isLogIn);
-  };
+    setIsLogIn((prev) => !prev)
+    console.log('swiched mode to', isLogIn)
+  }
 
   return (
-    <Card className="authentication">
+    <Card className='authentication'>
       <h2>Login Required</h2>
       <form onSubmit={authSubmitHandler}>
         {!isLogIn && (
           <Input
-            element="input"
-            id="name"
-            type="text"
-            label="Your name"
+            element='input'
+            id='name'
+            type='text'
+            label='Your name'
             validators={[VALIDATOR_REQUIRE]}
-            errorText="Please enter your name."
+            errorText='Please enter your name.'
             onInput={inputHandler}
           />
         )}
         <Input
-          element="input"
-          id="email"
-          type="email"
-          label="E-mail"
+          element='input'
+          id='email'
+          type='email'
+          label='E-mail'
           validators={[VALIDATOR_EMAIL]}
-          errorText="Please enter a valid email address."
+          errorText='Please enter a valid email address.'
           onInput={inputHandler}
         />
         <Input
-          element="input"
-          id="password"
-          type="password"
-          label="Password"
+          element='input'
+          id='password'
+          type='password'
+          label='Password'
           validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid password with at least 5 characters."
+          errorText='Please enter a valid password with at least 5 characters.'
           onInput={inputHandler}
         />
-        <Button type="submit" disabled={!formState.isValid}>
+        <Button type='submit' disabled={!formState.isValid}>
           LOGIN
         </Button>
       </form>
@@ -101,7 +133,7 @@ const Auth = () => {
         SIGN UP
       </Button>
     </Card>
-  );
-};
+  )
+}
 
-export default Auth;
+export default Auth
